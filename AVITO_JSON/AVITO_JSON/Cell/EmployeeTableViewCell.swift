35248b2d5fb45ba195c6fe298abebed844ac5efe
ZanLab <zanlabengine@gmail.com>
2021-09-09 19:09:01 +0300

    Interface added
    Created Table View Cell interface for json data

---
+++ AVITO_JSON/AVITO_JSON/Cell/EmployeeTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  EmployeeTableViewCell.swift
+//  AVITO_JSON
+//
+//  Created by admin on 09.09.2021.
+//  Copyright © 2021 ZanLabEngine. All rights reserved.
+//
+
+import UIKit
+
+class EmployeeTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var skillsLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    
+    
+    
+}
--- AVITO_JSON/AVITO_JSON/ViewController.swift
+++ AVITO_JSON/AVITO_JSON/ViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class EmployeesViewController: UIViewController {
 
+    
+    @IBOutlet var tableView: UITableView!
+    
+    
+        var currentCompany: Company? {
+            didSet {
+            currentCompany?.employees.sort {$0.name < $1.name }
+            tableView?.reloadData()
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+}
+    
+    extension EmployeesViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return currentCompany?.employees.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EmployeeCell") ?? UITableViewCell()
+        if let cell = cell as? EmployeeTableViewCell {
+            cell.nameLabel?.text = currentCompany?.employees[indexPath.row].name
+            cell.phoneLabel?.text = currentCompany?.employees[indexPath.row].phoneNumber
+            cell.skillsLabel?.text = currentCompany?.employees[indexPath.row].skills.joined(separator: ", ")
+        }
+        return cell
+    }
 
 }
+
 
